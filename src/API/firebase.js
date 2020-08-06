5da112c5f0af70d175e639a5eb90de98a5bf2389
mouad.zizi <mouadzizi@users.noreply.github.com>
2020-08-06 16:31:38 +0100

    commit firebase

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig={
-    apiKey: "AIzaSyDq4a8RVBoUah55Z_aHsu6ph0XsOl7j4ZU",
-    authDomain: "maxwin-289db.firebaseapp.com",
-    databaseURL: "https://maxwin-289db.firebaseio.com",
-    projectId: "maxwin-289db",
-    storageBucket: "maxwin-289db.appspot.com",
-    messagingSenderId: "1067066841229",
-    appId: "1:1067066841229:web:27c19a67a31a39e85b2ac4"
+    apiKey: "AIzaSyDHgZAAUP5oomuY2EE_N5mzcK2t-hgJNaQ",
+    authDomain: "maxwin-35d61.firebaseapp.com",
+    databaseURL: "https://maxwin-35d61.firebaseio.com",
+    projectId: "maxwin-35d61",
+    storageBucket: "maxwin-35d61.appspot.com",
+    messagingSenderId: "296245537422",
+    appId: "1:296245537422:web:8f10a04a61f99e206677df",
+    measurementId: "G-2LQX99YNHL"
 }
 
 const app = firebase.initializeApp(firebaseConfig);
