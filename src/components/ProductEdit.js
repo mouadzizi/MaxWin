1922963aef1ac234c8eaa4c59c5883333e428b9c
mouad.zizi <mouadzizi@users.noreply.github.com>
2020-09-19 15:06:23 +0100

    commit 52
    changes the profil, filtre, add content

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -1,49 +1,47 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import {GlobalStyle} from '../style/GlobalStyle';
-import {Fontisto, Entypo} from 'react-native-vector-icons'
+import {Entypo} from 'react-native-vector-icons';
 
 export default function ProductEdit(props) {
+  
+  const image1 = require('../../assets/produit.jpg')
+  const image2 = require('../../assets/produit3.png')
+
     return (
         
-        <View>
+
         
-        <TouchableOpacity
-        onPress={props.click}>
-        <View style={GlobalStyle.card}>
+        <View>
+        <View style={[GlobalStyle.card, {height: 80}]}>
           <View style={GlobalStyle.cardImgWrapper}>
             <Image
-              source={props.img}
+              source={image2}
               resizeMode="contain"
               style={GlobalStyle.cardImg}
             />
           </View>
           <View style={GlobalStyle.cardInfo}>
-            <Text style={GlobalStyle.cardTitle}>{props.name}</Text>
-            <Text style={GlobalStyle.cardPrice}>{props.price} MAD</Text>
-            <View
-            style={{flexDirection: 'row'}}>
-            <Entypo 
-              name='user'
-              color='#B9B9BB'
+            <Text style={GlobalStyle.cardTitle}>Product title</Text>
+            <Text style={GlobalStyle.cardPrice}>50 MAD</Text>
+          </View>
+          </View>
+          <Text></Text>
+          <View style={[GlobalStyle.card, {height: 80}]}>
+          <View style={GlobalStyle.cardImgWrapper}>
+            <Image
+              source={image1}
+              resizeMode="contain"
+              style={GlobalStyle.cardImg}
             />
-            <Text style={GlobalStyle.cardOwner}>{props.owner}</Text>
-            </View>
-
-            <View
-            style={{flexDirection: 'row', marginTop: 2}}>
-            <Entypo
-            name='location'
-            size={15}
-            color='#B9B9BB'
-            />
-            <Text style={GlobalStyle.cardLocation}>{props.location}</Text>
-            </View>
-            <Text style={GlobalStyle.cardState}>{props.state}</Text>
+          </View>
+          <View style={GlobalStyle.cardInfo}>
+            <Text style={GlobalStyle.cardTitle}>Product title 2</Text>
+            <Text style={GlobalStyle.cardPrice}>300 MAD</Text>
+          </View>
           </View>
         </View>
-      </TouchableOpacity>
 
-      </View>
+
     )
 }
