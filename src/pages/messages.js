6ded034852e56948206f1f4057f084a86e6383b9
moussa23 <mosawi.man@live.fr>
2020-11-03 19:06:07 +0100

    hhh

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -1,16 +1,82 @@
 import React from 'react'
 import { View, Text, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { db, auth } from '../API/firebase';
+import firebase from 'firebase';
 
 export default function messages() {
-   
-    return (
-      
-        <View style={{ flex: 1 }}>
-        <GiftedChat />
-        {
-           Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
+
+    const [messages, setMessages] = React.useState([]);
+    const [messageSent, setMessageSent] = React.useState(false);
+    var chatRef = null
+    React.useEffect(() => {
+
+         loadMessages();
+        return () => {
+            console.log('chats exite');
+            chatRef()
         }
-     </View>
+    }, [])
+
+    const onSend = React.useCallback(async (messages = []) => {
+        for (let i = 0; i < messages.length; i++) {
+            await db.collection('chats')
+                .doc('chatID')
+                .collection('messages')
+                .add({
+                    _id: messages[i]._id,
+                    text: messages[i].text,
+                    user: messages[i].user,
+                    sent: true,
+                    createdAt: messages[i].createdAt,
+                }).then(() => console.log('messageSent'))
+                .catch(e => console.log(e.message));
+        }
+
+
+    }, [])
+
+    const loadMessages = () => {
+        chatRef = db.collection('chats').doc('chatID').collection('messages').orderBy('createdAt', 'desc')
+            .onSnapshot(s => {
+                var data = [];
+                s.docChanges().forEach((change) => {
+                    if (change.type == 'added') {
+                        console.log('====================================');
+                        console.log('data added');
+                        console.log('====================================');
+                        try {
+                             data.push({
+                                _id: change.doc.data()._id,
+                                createdAt: new Date(change.doc.data().createdAt.seconds * 1000),
+                                sent: change.doc.data().sent,
+                                text: change.doc.data().text,
+                                user: change.doc.data().user
+                            });
+                        } catch (error) {
+                            console.warn(error.message);
+                        }
+
+                    }
+                });
+                setMessages(data);
+            })
+    }
+
+    // const  chatId=()=> {
+    //     if (auth.currentUser.uid > route.params.item.key) return `${auth.currentUser.uid}-${route.params.item.key}`
+    //     else return `${route.params.item.key}-${auth.currentUser.uid}`;}
+
+    return (
+
+        <View style={{ flex: 1 }}>
+            <GiftedChat
+                messages={messages}
+                onSend={message => onSend(message)}
+                user={{
+                    id: auth.currentUser.uid,
+                    name: auth.currentUser.displayName
+                }} />
+        </View>
     )
 }
